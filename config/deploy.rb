f7f4b04bd711840499e38fafc41c1418d2d550a5
ampatel2 <ampatel2@gmail.com>
2017-10-24 10:35:29 -0500

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ namespace :deploy do
   desc "reload the database with seed data"
   task :seed do
     puts "==Dev Tasks: Seeding Database=="
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{:stage}"
   end
 
   before :starting,     :check_revision
